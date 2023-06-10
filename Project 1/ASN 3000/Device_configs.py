c62db90228ee89bc93d65854ac2e1f9a3cc5fb8f
NGMunia <anthony.ngure@yahoo.com>
2023-06-10 14:28:31 +0300

    projectd

---
+++ Project 1/ASN 3000/Device_configs.py
@@ -0,0 +1,69 @@
+#BGP configuration:
+from netmiko import ConnectHandler
+
+#C2000
+from Device_list import C3000 #Route reflector 
+net_connect=ConnectHandler(**C3000)
+net_connect.enable()
+bgp_comms=["router bgp 3000",
+           "neighbor 1.1.1.1 remote-as 3000",
+           "neighbor 1.1.1.1 update-source lo0",
+           "neighbor 1.1.1.1 route-reflector-client",
+           "neighbor 2.2.2.2 remote-as 3000",
+           "neighbor 2.2.2.2 update-source lo0",
+           "neighbor 2.2.2.2 route-reflector-client"
+           "neighbor 4.4.4.4 remote-as 3000",
+           "neighbor 4.4.4.4 update-source lo0",
+           "neighbor 4.4.4.4 route-reflector-client"
+           "neighbor 5.5.5.5 remote-as 3000",
+           "neighbor 5.5.5.5 update-source lo0"
+           "neighbor 5.5.5.5 route-reflector-client"]
+ouput=net_connect.send_config_set(bgp_comms)
+net_connect.save_config()
+net_connect.disconnect()
+
+
+#A2000
+from Device_list import A3000
+net_connect=ConnectHandler(**A3000)
+net_connect.enable()
+bgp_comms=["router bgp 3000",
+           "neighbor 3.3.3.3 remote-as 2000",
+           "neighbor 3.3.3.3 update-source lo0",
+           "neighbor 3.3.3.3 next-hop-self",
+           "neighbor 172.128.4.1 remote-as 2000"]
+ouput=net_connect.send_config_set(bgp_comms)
+net_connect.save_config()
+net_connect.disconnect()
+
+
+
+#D2000
+from Device_list import D3000
+net_connect=ConnectHandler(**D3000)
+net_connect.enable()
+bgp_comms=["router bgp 3000",
+           "neighbor 3.3.3.3 remote-as 2000",
+           "neighbor 3.3.3.3 update-source lo0",
+           "network 44.67.28.0 mask 255.255.255.252",
+           "neighbor 44.67.28.2 remote-as 4000",
+           "aggregate-address 44.67.28.0 255.255.255.0 summary-only"]
+ouput=net_connect.send_config_set(bgp_comms)
+net_connect.save_config()
+net_connect.disconnect()
+
+
+#E2000
+from Device_list import E3000
+net_connect=ConnectHandler(**E3000)
+net_connect.enable()
+bgp_comms=["router bgp 3000",
+           "neighbor 3.3.3.3 remote-as 2000",
+           "neighbor 3.3.3.3 update-source lo0",
+           "neighbor 3.3.3.3 next-hop-self",
+           "network 32.19.86.0 mask 255.255.255.252",
+           "aggregate-address 32.19.86.0 255.255.255.0 summary-only"]
+ouput=net_connect.send_config_set(bgp_comms)
+net_connect.save_config()
+net_connect.disconnect()
+
---
+++ Project 1/ASN 3000/Device_list.py
@@ -0,0 +1,31 @@
+#Device list ASN-3000:
+
+A3000={"device_type": "cisco_ios",
+       "hostname": "1.1.1.1",
+       "username": "Automation",
+       "password": "cisco123",
+       "secret": "cisco123"}
+
+B3000={"device_type": "cisco_ios",
+       "hostname": "2.2.2.2",
+       "username": "Automation",
+       "password": "cisco123",
+       "secret": "cisco123"}
+
+C3000={"device_type": "cisco_ios",
+       "hostname": "3.3.3.3",
+       "username": "Automation",
+       "password": "cisco123",
+       "secret": "cisco123"}
+
+D3000={"device_type": "cisco_ios",
+       "hostname": "4.4.4.4",
+       "username": "Automation",
+       "password": "cisco123",
+       "secret": "cisco123"}
+
+E3000={"device_type": "cisco_ios",
+       "hostname": "5.5.5.5",
+       "username": "Automation",
+       "password": "cisco123",
+       "secret": "cisco123"}
