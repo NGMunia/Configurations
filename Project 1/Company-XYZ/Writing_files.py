3d1332984581174c0d5eb08777fc375a6072ba22
NGMunia <anthony.ngure@yahoo.com>
2023-06-10 12:44:41 +0300

    projects

--- Project 1/Company-XYZ/Writing_files.py
+++ Project 1/Company-XYZ/Writing_files.py
@@ -1,14 +1,14 @@
 #COMMON CONFIGS:
 
 #SNMP configuration:
-with open("C:\\Users\\Munia-Virtual\\Desktop\\Scripts\\Configurations\\Project 1\\SNMP.txt","w") as f:
+with open("C:\\Users\\Munia-Virtual\\Desktop\\Scripts\\Configurations\\Project 1\\Company-XYZ\\SNMP.txt","w") as f:
     f.write("ip access-list standard snmp-acl\n")
     f.write("permit host 192.168.255.254\n")
     f.write("snmp-server system-shutdown\n")
     f.write("snmp-server community xyz_routers ro snmp-acl\n")
     f.write("snmp-server enable traps config\n")
     f.write("snmp-server host 192.168.255.254 traps version 2c xyz_routers\n")
-with open("C:\\Users\\Munia-Virtual\\Desktop\\Scripts\\Configurations\\Project 1\\SNMP.txt","r") as f:
+with open("C:\\Users\\Munia-Virtual\\Desktop\\Scripts\\Configurations\\Project 1\\Company-XYZ\\SNMP.txt","r") as f:
     read_data=f.read()
     print(read_data)
 
