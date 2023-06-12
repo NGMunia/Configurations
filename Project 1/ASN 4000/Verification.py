d124a08ecd10d3a6d72ba55f27de13b514e7aabf
NGMunia <anthony.ngure@yahoo.com>
2023-06-12 08:38:41 +0300

    projects

--- Project 1/ASN 4000/Verification.py
+++ Project 1/ASN 4000/Verification.py
@@ -3,8 +3,8 @@ from netmiko import ConnectHandler
 from Device_list import B4000
 net_connect=ConnectHandler(**B4000)
 net_connect.enable()
-commands=["show ip bgp","show ip bgp summary"]
-for i in commands:
+show_commands=["show ip bgp","show ip bgp summary"]
+for i in show_commands:
     output=net_connect.send_command(i)
     print("\n")
     print(output)
