1176707def5e650fe698c3810e0f4b15206239de
NGMunia <anthony.ngure@yahoo.com>
2023-06-12 19:22:22 +0300

    projects

--- Project 1/Company-XYZ/Device_list.py
+++ Project 1/Company-XYZ/Device_list.py
@@ -8,47 +8,47 @@ Hub=    {"device_type": "cisco_ios",
 LAN=    {"device_type": "cisco_ios",
          "host": "192.168.10.1",
          "username": "Automation",
-         "pasword": "cisco123",
+         "password": "cisco123",
          "secret": "cisco123" }
 
 Firewall={
          "device_type": "cisco_ios",
          "host": "10.1.1.1",
          "username": "Automation",
-         "pasword": "cisco123",
+         "password": "cisco123",
          "secret": "cisco123"}
 
 Edge=    {"device_type": "cisco_ios",
          "host": "10.1.1.10",
          "username": "Automation",
-         "pasword": "cisco123",
+         "password": "cisco123",
          "secret": "cisco123" }
 
 Spokes=[{"device_type": "cisco_ios",
          "host": "172.31.1.20",
          "username": "Automation",
-         "pasword": "cisco123",
+         "password": "cisco123",
          "secret": "cisco123"
         },
         {
          "device_type": "cisco_ios",
          "host": "172.31.1.30",
          "username": "Automation",
-         "pasword": "cisco123",
+         "password": "cisco123",
          "secret": "cisco123"
         },
         {
          "device_type": "cisco_ios",
          "host": "172.31.1.40",
          "username": "Automation",
-         "pasword": "cisco123",
+         "password": "cisco123",
          "secret": "cisco123" 
         },
         {
          "device_type": "cisco_ios",
          "host": "172.31.1.255",
          "username": "Automation",
-         "pasword": "cisco123",
+         "password": "cisco123",
          "secret": "cisco123"   
         }
         ]
