fb48f56be4cce933fdc6a6a8c165a2ea4fa4f0b9
NGMunia <anthony.ngure@yahoo.com>
2023-06-16 15:55:44 +0300

    projects

--- Project 1/Company-XYZ/verification.py
+++ Project 1/Company-XYZ/verification.py
@@ -1,18 +1,18 @@
 
 from netmiko import ConnectHandler
 from Device_list import R1_Edge, firewall, R1_HUB, R1_LAN
+from rich import print as rprint
 
 
 print("=====verifying Edge router=====\n")
 net_connect=ConnectHandler(**R1_Edge)
-commands=["show ntp status", "show ip ospf neighbor", "show ip nat translations"]
-for command in commands:
-    print(net_connect.send_command(command)+"\n")
+net_connect.enable()
+commands=input("Edge-Router\ninput show command: ")
+rprint(net_connect._send_command_str(commands)+"\n")
     
 
 print("=====Verifying Firewall Router=====\n")
 net_connect=ConnectHandler(**firewall)
 net_connect.enable()
-commands=["show policy-map type inspect zone-pair","show ip access-lists"]
-for commands in commands:
-    print(net_connect.send_command(commands))
+commands=input("Firewall-Router\ninput show command: ")
+rprint(net_connect.send_command(commands))
