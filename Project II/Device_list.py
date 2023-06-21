2a99b086dda5e12830fcb83e9a7bbe283d4a859a
NGMunia <anthony.ngure@yahoo.com>
2023-06-21 20:45:11 +0300

    projects

---
+++ Project II/Device_list.py
@@ -0,0 +1,24 @@
+
+R1   =  {
+            "device_info":"cisco_ios",
+            "host": "192.168.10.1",
+            "username":"Automation",
+            "secret": "cisco123",
+            "password":"cisco123"
+        }
+
+R2   =  {
+            "device_info":"cisco_ios",
+            "host": "192.168.20.1",
+            "username":"Automation",
+            "secret": "cisco123",
+            "password":"cisco123"
+        }
+
+R3   =  {
+            "device_info":"cisco_ios",
+            "host": "192.168.30.1",
+            "username":"Automation",
+            "secret": "cisco123",
+            "password":"cisco123"
+        }
