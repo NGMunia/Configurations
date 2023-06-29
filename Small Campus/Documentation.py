9177389abdfb4c857b92ad6e727d6c0aaff125e6
NGMunia <anthony.ngure@yahoo.com>
2023-06-29 08:45:16 +0300

    projects

--- Small Campus/Documentation.py
+++ Small Campus/Documentation.py
@@ -1,68 +1,22 @@
 
-import csv
 from netmiko import ConnectHandler
-from Device_list import access, core_sw
+from Device_list import R1_Edge,core_sw, FW_1
+import csv
 
+#Documenting Serial numbers, Image, and Version
 
-
-#Documenting Access_switches' interfaces and switchport access status:
-with open('C:\\Users\\Munia-virtual\\Desktop\\Scripts\\Configurations\\Small Campus\\access_switches.csv','w') as f:
-    write_data = csv.writer(f)
-    write_data.writerow(['hostname','interface','switchport'])
-    
-    for switches in access:
-        net_connect=ConnectHandler(**switches)
-        net_connect.enable()
+with open('C:\\Users\\Munia-virtual\\Desktop\\Scripts\\Configurations\\Small Campus\\Routers.csv','w',newline='') as f:
+        write_data = csv.writer(f)
+        write_data.writerow(['Hostname','Version','Image','Serial'])
         
-        hostname = net_connect.send_command("show run | include hostname")
-        write_data.writerow([hostname])
+        for devices in R1_Edge,core_sw,FW_1: 
+            net_connect=ConnectHandler(**devices)
+            net_connect.enable()
 
-        for i in range(0,4):
-            interface  = net_connect.send_command("show run | include interface Ethernet0/"+str(i))
-            switchport = net_connect.send_command("show run interface ethernet0/"+str(i)+" | include switchport access")
-            write_data.writerow(['',interface,switchport])
-
-
-#Documenting Core SW:
-with open('C:\\Users\\Munia-virtual\\Desktop\\Scripts\\Configurations\\Small Campus\\Core_sw.csv','w') as f:
-    net_connect=ConnectHandler(**core_sw)
-    net_connect.enable()
-
-    write_data=csv.writer(f)
-    write_data.writerow(['hostname','interface','switchport status','Address'])
-    hostname = net_connect.send_command("show run | include hostname")
-    write_data.writerow([hostname])
-
-    for v in range (10,50,10):
-        interface  = net_connect.send_command("show run | include Vlan"+str(v))
-        ip_addr    = net_connect.send_command("show run int Vlan"+str(v)+" | include ip address")
-        write_data.writerow(['',interface,'',ip_addr])
-    
-    for i in range (0,4):
-        trunk_intf = net_connect.send_command("show run | include interface Ethernet0/"+str(i))
-        port_status= net_connect.send_command("sh running-config interface e0/"+str(i)+" | include switchport mode")
-        write_data.writerow(['',trunk_intf,port_status,''])
-
-
-
-#Documenting VLANs on access switches:
-with open('C:\\Users\\Munia-virtual\\Desktop\\Scripts\\Configurations\\Small Campus\\Access_sw_vlans.csv','w',newline='') as f:
-    write_data = csv.writer(f)
-    write_data.writerow(['Hostname','IP address','VLANs'])
-
-    for switches in access:
-        net_connect=ConnectHandler(**switches)
-        net_connect.enable()
-
-        output     = net_connect.send_command("show vlan brief")+'\n\n\n'
-        hostname   = net_connect.send_command("show run | include hostname")
-        ip_address = (f'{switches.get("ip")}')
-
-        write_data.writerow([hostname,ip_address,output])
-
-
-
-
-
-
-
+            hostname = net_connect.send_command("show run | include hostname")
+            output = net_connect.send_command("show version",use_textfsm=True)[0]
+            version= output.get("version")
+            image = output.get("running_image")
+            serial = output.get("serial")
+            
+            write_data.writerow([hostname,version,image,serial])
